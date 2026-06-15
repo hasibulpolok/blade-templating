eaf683cc5476f22915148ec9de615c0d49fe3d0b
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-06-15 10:54:21 +0600

    footer content change

--- resources/views/backend/parts/footer.blade.php
+++ resources/views/backend/parts/footer.blade.php
@@ -1,6 +1,6 @@
     <footer class="admin-footer">
         <div class="container-fluid px-3 px-lg-4">
-          <span>Copyright 2026 adminHMD. <br> Developed by <a target="_blank" class="fw-bold text-success" href="https://github.com/HasanMahmudDev">Md. Hasan Mahmud</a> • Distributed by <a target="_blank" class="fw-bold text-success" href="https://themewagon.com">ThemeWagon</a> </span>
+          <span>Copyright 2026 adminHMD. <br> Developed by Md Hasibul Bashar Polok  </span>
           <span>Professional dashboard template.</span>
         </div>
       </footer>
